37c1396aadc3782520845aee63299572156eb1fc
Vinay S <vinayshankar0369@gmail.com>
2023-08-04 20:08:52 +0530

    modified files

--- backend/views.py
+++ backend/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import ContactMessage, user_details
 
 
 def landing(request):
@@ -22,6 +23,22 @@ def appointments(request):
 
 
 def contacts(request):
+    # return render(request, 'contacts.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        # Save the form data to the database
+        contact_message = ContactMessage(
+            name=name, email=email, phone=phone, subject=subject, message=message)
+        contact_message.save()
+
+        # Redirect to the success page
+        return redirect('contact_success')
+
     return render(request, 'contacts.html')
 
 
@@ -35,3 +52,29 @@ def dashboard(request):
 
 def admin_login(request):
     return render(request, 'login-admin.html')
+
+
+def logincon(request):
+    username = request.POST('username')
+    password = request.POST('password')
+    user = user_details.objects.get(username=username)
+    if user.password == password:
+        return redirect('/home')
+    else:
+        return redirect('/login')
+
+
+def signup(request):
+    username = request.POST('username')
+    password = request.POST('password')
+    email = request.POST('email')
+    try:
+
+        user = user_details.objects.get(username=username)
+        lol = user_details(username=username, password=password, email=email)
+        lol.save()
+        print('saved')
+        return redirect('/login')
+    except:
+        print('no')
+        return redirect('/login')
